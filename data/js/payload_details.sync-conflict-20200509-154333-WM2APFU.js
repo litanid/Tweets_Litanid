5f5b1ee607b07b5c2aea432033e034c9a93bf2f1
Litanid <litanid@gmail.com>
2022-04-06 23:21:58 +0800

    modify

--- data/js/payload_details.sync-conflict-20200509-154333-WM2APFU.js
+++
@@ -1,6 +0,0 @@
-var payload_details = {
-  "tweets": 1652,
-  "created_at": "2017-08-23 07:44:31 +0000",
-  "lang": "zh-CN",
-  "updated_at": "Wed May 06 16:00:01 +0000 2020"
-}
--- data/js/payload_details.sync-conflict-20220227-201347-XFZO43F.js
+++
@@ -1,6 +0,0 @@
-var payload_details = {
-  "tweets": 2002,
-  "created_at": "2017-08-23 07:44:31 +0000",
-  "lang": "zh-CN",
-  "updated_at": "Tue Aug 24 16:00:15 +0000 2021"
-}
